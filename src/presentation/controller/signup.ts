75ba6b0f6a7ae21fb5700ea0abf5a36cdd7a6c14
nicasiomarques <nicasiomarques18@gmail.com>
2021-08-22 18:49:31 +0100

    feat: return 400 if password is diffferent of the passwordConfirm

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -18,6 +18,10 @@ export class SignUpController implements Controller {
         }
       }
 
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirm) {
+        return badRequest(new InvalidParamError('passwordConfirm'))
+      }
+
       const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
       if (!emailIsValid) {
         return badRequest(new InvalidParamError('email'))
