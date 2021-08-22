6c035967980fcb3ab0190266087515cd113d8123
nicasiomarques <nicasiomarques18@gmail.com>
2021-08-22 13:48:15 +0100

    fix: makes SignUpController implements correct interface

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,8 +1,9 @@
 import { MissingParamError } from '../errors/missing-param-error'
+import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { badRequest } from './helpers/http-helpers'
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirm']
     for (const field of requiredFields) {
