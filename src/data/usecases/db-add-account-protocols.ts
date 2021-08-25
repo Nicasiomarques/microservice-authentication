20ad57bd32ae6340d37593d805dbd8e3f1581c5c
nicasiomarques <nicasiomarques18@gmail.com>
2021-08-25 11:00:52 +0100

    feat: ensure DbAddAccount throw if add AccountRepository.add Throws

--- src/data/usecases/db-add-account-protocols.ts
+++ src/data/usecases/db-add-account-protocols.ts
@@ -1,4 +1,5 @@
+export * from '../protocols/add-account-repository'
 export * from '../../domain/usecases/add-account'
-export * from "../../domain/models/account"
+export * from '../../domain/models/account'
 export * from '../protocols/encrypter'
 export * from './db-add-account'
--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -1,10 +1,13 @@
-import { AccountModel, Encrypter, AddAccountModel, AddAccount } from "./db-add-account-protocols"
+import { AccountModel, AddAccountRepository, Encrypter, AddAccountModel, AddAccount } from './db-add-account-protocols'
 export class DbAddAccount implements AddAccount {
-  constructor(private readonly encrypter: Encrypter) {
-		this.encrypter = encrypter
-	}
-  async add (account: AddAccountModel): Promise<AccountModel> {
-		await this.encrypter.encrypt(account.password)
-		return Promise.resolve(null)
-	}
+  constructor (
+    private readonly encrypter: Encrypter,
+    private readonly addAccountRepository: AddAccountRepository
+  ) {}
+
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password)
+    const account = await this.addAccountRepository.add({ ...accountData, password: hashedPassword })
+    return account
+  }
 }
