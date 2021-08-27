34519d9583b4cf5b6a2b1f0e5d01421ba5a324b2
nicasiomarques <nicasiomarques18@gmail.com>
2021-08-27 16:11:15 +0100

    test: ensure default content type is json and change when forced

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,27 @@
+import request from 'supertest'
+import app from '../app'
+
+describe('content type middleware', () => {
+  it('Should return as default content type json', async () => {
+    const endpoint = '/any_url'
+
+    app.get(endpoint, (_, response) => response.send())
+
+    await request(app)
+      .get(endpoint)
+      .expect('content-type', /json/)
+  })
+
+  it('Should return xml as content type when forced', async () => {
+    const endpoint = '/any_url2'
+
+    app.get(endpoint, (_, response) => {
+      response.type('xml')
+      response.send()
+    })
+
+    await request(app)
+      .get(endpoint)
+      .expect('content-type', /xml/)
+  })
+})
