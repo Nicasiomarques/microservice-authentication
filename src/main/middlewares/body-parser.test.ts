579fd416736ed6e904cb173b9d4e76923c60d028
nicasiomarques <nicasiomarques18@gmail.com>
2021-08-27 15:29:11 +0100

    test: ensure request is parsed to json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest'
+import app from '../app'
+
+describe('bodyParser middleware', () => {
+  it('should parse payload as json correctly', async () => {
+    const payload = { name: 'any_name' }
+    const endpoint = '/any_post_url'
+
+    app.post(endpoint, (request, response) =>
+      response.send(request.body)
+    )
+
+    await request(app)
+      .post(endpoint)
+      .send(payload)
+      .expect(payload)
+  })
+})
