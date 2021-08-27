c0b16f4c8455e17ad31da746b4e3cbe57f1a7be1
nicasiomarques <nicasiomarques18@gmail.com>
2021-08-27 17:16:23 +0100

    refactor: connect databse berfore integration

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,20 @@
 import request from 'supertest'
+import { MongoHelpers } from '../../infra/db/mongodb/helpers'
 import app from '../config/app'
 
 describe('SignUp routes', () => {
+  beforeAll(async () => {
+    await MongoHelpers.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelpers.disconnect()
+  })
+
+  beforeEach(async () => {
+    MongoHelpers.getCollection('accounts').deleteMany({})
+  })
+
   it('should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
